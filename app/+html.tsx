d16af317bc142eba0faa79d59cd2d889a954a055
jewe1V <zensot1@yandex.ru>
2026-04-26 17:46:00 +0500

    fix layout for pwa

--- app/+html.tsx
+++ app/+html.tsx
@@ -5,13 +5,36 @@ export default function Root({ children }: { children: React.ReactNode }) {
     return (
         <html lang="ru">
         <head>
-            <meta charSet="utf-8" />
-            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+            <meta charSet="utf-8"/>
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"/>
 
-            <link rel="manifest" href="/pwa/manifest.json" />
+            <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 
-            <ScrollViewStyleReset />
+            <link rel="manifest" href="/pwa/manifest.json"/>
+
+            <ScrollViewStyleReset/>
+
+            <style dangerouslySetInnerHTML={{ __html: `
+                html, body {
+                    overflow: hidden;
+                    height: 100%;
+                    width: 100%;
+                    position: fixed;
+                    -webkit-tap-highlight-color: transparent;
+                    background-color: #ffffff;
+                    margin: 0;
+                    padding: 0;
+                }
+    
+                #root {
+                    height: 100%;
+                    width: 100%;
+                    overflow-y: auto;
+                    -webkit-overflow-scrolling: touch;
+                }
+            `}} />
         </head>
         <body>{children}</body>
         </html>
